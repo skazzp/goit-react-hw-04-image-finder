9ee44964b21a54a6825ac7049c958ead6a1d96b5
skazzp <skazzp@gmail.com>
2022-09-24 21:01:25 +0300

    cleaning

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,7 +2,6 @@ import { ModalPic, Overlay } from './Modal.styled';
 import { useState, useEffect } from 'react';
 
 const Modal = ({ toggleModal, largeImageURL }) => {
-  // const [isLoading, setIsLoading] = useState(true);
   const [isOpen, setIsOpen] = useState(true);
 
   useEffect(() => {
@@ -34,36 +33,3 @@ const Modal = ({ toggleModal, largeImageURL }) => {
 };
 
 export default Modal;
-
-// class Modal extends Component {
-//   state = {
-//     isLoading: true,
-//   };
-
-//   componentDidMount() {
-//     window.addEventListener('keydown', this.handleClose);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener('keydown', this.handleClose);
-//   }
-
-//   handleClose = e => {
-//     if (e.target === e.currentTarget || e.code === 'Escape') {
-//       this.props.toggleModal();
-//     }
-//   };
-
-//   render() {
-//     const { largeImageURL } = this.props;
-//     return (
-//       <Overlay onClick={this.handleClose}>
-//         <ModalPic>
-//           <img src={largeImageURL} alt="" />
-//         </ModalPic>
-//       </Overlay>
-//     );
-//   }
-// }
-
-// export default Modal;
